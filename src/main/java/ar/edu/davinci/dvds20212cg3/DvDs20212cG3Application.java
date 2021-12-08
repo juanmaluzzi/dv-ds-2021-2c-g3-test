f8e6834d33e5d2ca47263dc20c977c16b09b7be6
francomastrantonio <70065323+francomastrantonio@users.noreply.github.com>
2021-12-08 19:46:28 -0300

    tp base
    Signed-off-by: francomastrantonio <70065323+francomastrantonio@users.noreply.github.com>

---
+++ src/main/java/ar/edu/davinci/dvds20212cg3/DvDs20212cG3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20212cg3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20212cG3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20212cG3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20212cg3/DvDs20212cG3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20212cg3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20212cG3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
